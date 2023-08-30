03cd5263868fbafd2745203e8901fefe3766d191
sachico-bull <135416262+sachico-bull@users.noreply.github.com>
2023-08-30 09:02:45 +0900

    課題  条件分岐を使用して倍数を表示しよう
    課題を作成しました。
    ご確認よろしくお願いいたします。

---
+++ kadai_007/calc.js
@@ -0,0 +1,26 @@
+let num = Math.floor(Math.random() * 16);
+
+console.log(num);
+
+switch (num) {
+    case 3:
+        console.log('3の倍数です');
+        break;
+    case 5:
+        console.log('5の倍数です');
+        break;
+    case 6:
+        console.log('3の倍数です');
+        break;
+    case 9:
+        console.log('3の倍数です');
+        break;
+    case 10:
+        console.log('5の倍数です');
+        break;
+    case 12:
+        console.log('3の倍数です');
+        break;        
+    case 15:
+        console.log('3と5の倍数です');
+}
